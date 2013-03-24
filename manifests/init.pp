84a48341a7cdeeaffe74c46c397c4c8b7b04beaa
Florian Weber <florian@webflo.org>
2013-03-24 22:42:56 +0100

    Fixed typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
-# Public: Install Chrome to /Applications.
+# Public: Install SourceTree to /Applications.
 #
 # Examples
 #
-#   include chrome
+#   include sourcetree
 class sourcetree {
   package { 'SourceTree':
     source   => 'http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.7.1.dmg',
